2cdacf5903faec3328adc8af7055d4deecf36649
alessandrodanzi <62661649+alessandrodanzi@users.noreply.github.com>
2020-06-11 16:34:18 +0200

    Create R_Project_monitoring_ecosystems.r

---
+++ R_Project_monitoring_ecosystems.r
@@ -0,0 +1,6 @@
+#R_Project_monitoring_ecosystems
+#climate change is acting as fast modification in the ecosystems.
+#Higher temperatures are melting the snow and giving more space to the vegetation to grow direction north.
+#The albedo of the snow is higher then the one of the grass that is higher than the one of the trees
+#We would expect, during the years, a reduction in the albedo, with a progressively movement of the line of the vegetation going towards the Arctic
+setwd("/Users/alessandro/lab/climatechange")
