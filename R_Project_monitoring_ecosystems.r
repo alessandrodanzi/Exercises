0252dffd723386db536070160daf202bf2ec1673
alessandrodanzi <62661649+alessandrodanzi@users.noreply.github.com>
2020-06-12 10:56:45 +0200

    Update R_Project_monitoring_ecosystems.r

--- R_Project_monitoring_ecosystems.r
+++ R_Project_monitoring_ecosystems.r
@@ -4,3 +4,15 @@
 #The albedo of the snow is higher then the one of the grass that is higher than the one of the trees
 #We would expect, during the years, a reduction in the albedo, with a progressively movement of the line of the vegetation going towards the Arctic
 setwd("/Users/alessandro/lab/climatechange")
+library(raster)
+library(ncdf4)
+
+
+albedojune2020 <-brick("c_gls_ALDH_202005240000_GLOBE_PROBAV_V1.5.1.nc")
+
+cl <- colorRampPalette(c('darkblue','dark green','yellow'))(100) 
+plot(albedojune2020,col=cl)
+#the file seems heavy, let's try to reduce the size through the function aggregate
+albedojune2020_10<- aggregate (albedojune2020,fact=10)
+plot(albedojune2020_10,col=cl)
+
